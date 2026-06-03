b5720a8c86fdcb67fbf46312ed8c50dec02ee488
Sakshyam-RT14 <pokhrelsakshyam8@gmail.com>
2026-06-03 18:03:35 +0545

    initial commit

---
+++ Day 03/main.py
@@ -0,0 +1,19 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client(api_key= os.getenv("GEMINI_API_KEY"))
+
+
+response = client.models.generate_content(
+    model = "gemini-2.5-flash",
+    contents = "Explain recursion",
+    config = {
+            "system_instruction": "You are a begineer tutor teaching computer science always keep the answers simple in 3 lines"
+    }
+    
+)
+
+print(response.text)
